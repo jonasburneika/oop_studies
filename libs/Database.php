c4237bd2ecaee3cfef1cd0b73c3750984f6b8f29
jonasburneika <jonas.burneika@gmail.com>
2019-01-18 20:06:20 +0200

    database libs update

--- libs/Database.php
+++ libs/Database.php
@@ -19,9 +19,26 @@ class Database
         }
     }
 
-    public function select($target = '*')
+    public function select($columns = '*', $distinct = false)
     {
-        $this->sqlQuery .= 'SELECT ' . $target . ' ';
+       
+        if (!is_array($columns)){
+            $this->sqlQuery .= 'SELECT ' . $columns . ' ';
+        } else {
+            $i = 0;
+            foreach ($columns as $column) {
+                if ($i > 0) {
+                    $this->sqlQuery .= $column . ' ';
+                } else {
+                    $this->sqlQuery .= 'SELECT ' . $column . ', ';
+                }
+                if ($i < count($columns)) { 
+                    $this->sqlQuery .= ', ';
+                }
+                $i++;
+            }
+        }
+       
         return $this;
     }
 
@@ -30,7 +47,18 @@ class Database
         $this->sqlQuery .= ' INTO ' . $tableName . ' ';
         return $this;
     }
+
+    public function delete()
+    {
+        $this->sqlQuery .= 'DELETE ' ;
+        return $this;
+    }   
     
+    public function update($tableName)
+    {
+        $this->sqlQuery .= 'UPDATE ' . $tableName . ' ';
+        return $this;
+    }
     
     public function from($tableName)
     {
@@ -38,28 +66,99 @@ class Database
         return $this;
     }
 
-    public function where($field, $value, $operator = '=')
+    public function set($values)
     {
-        $this->sqlQuery .= 'WHERE ' . $field . ' ' . $operator  . ' ' . $value . ' ';
+        if (!is_array($values)){
+            return false;
+        }
+        $this->sqlQuery .= ' SET ';
+        $i = 0;
+        foreach ($values as $key => $values) {
+            $this->sqlQuery .= ' '. $key . ' = ' . $values . ' ';
+            if ($i < count($values)){
+                $this->sqlQuery .= ', ';
+            }
+            $i++;
+        }
         return $this;
     }
 
-    public function whereAnd($field, $value, $operator = '=')
+    public function where($columnValue, $value, $operator = '=')
     {
-        $this->sqlQuery .= 'AND ' . $field . ' ' . $operator  . ' ' . $value . ' ';
+        $this->sqlQuery .= 'WHERE ' . $columnValue . ' ' . $operator  . ' ' . $value . ' ';
         return $this;
     }
 
-    public function whereOR($field, $value, $operator = '=')
+    public function whereAnd($columnValue, $value, $operator = '=')
     {
-        $this->sqlQuery .= 'OR ' . $field . ' ' . $operator  . ' ' . $value . ' ';
+        $this->sqlQuery .= ' AND ' . $columnValue . ' ' . $operator  . ' ' . $value . ' ';
         return $this;
+    }
+
+    public function whereOr($columnValue, $value, $operator = '=')
+    {
+        $this->sqlQuery .= ' OR ' . $columnValue . ' ' . $operator  . ' ' . $value . ' ';
+        return $this;
+    }
+
+    public function whereNot($columnValue, $value, $operator = '=')
+    {
+        $this->sqlQuery .= ' NOT ' . $columnValue . ' ' . $operator  . ' ' . $value . ' ';
+        return $this;
+    }
+
+    public function whereBetween($columnValue, $value1, $value2 )
+    {
+        $this->sqlQuery .= ' WHERE ' . $columnValue . ' BETWEEN ' . $value1  . ' AND ' . $value2 . ' ';
+        return $this;
+    }
+    
+    public function in($sql)
+    {
+        if (is_string($sql)){
+            $this->sqlQuery .= ' IN ( ' . $sql . ') ';
+            return $this;
+        }
+        if (is_array($sql)){
+            $this->sqlQuery .= ' IN ( ';
+            $i = 0;
+            foreach ($sql as $value) {
+                $this->sqlQuery .= $value;
+                if ($i < count($sql)){
+                    $this->sqlQuery .= ', ';
+                }
+            }
+            $this->sqlQuery .= ') ';
+            return $this;
+        }
+        
+    }
+
+    public function notIn($sql)
+    {
+        if (is_string($sql)){
+            $this->sqlQuery .= ' NOT IN ( ' . $sql . ') ';
+            return $this;
+        }
+        if (is_array($sql)){
+            $this->sqlQuery .= ' NOT IN ( ';
+            $i = 0;
+            foreach ($sql as $value) {
+                $this->sqlQuery .= $value;
+                if ($i < count($sql)){
+                    $this->sqlQuery .= ', ';
+                }
+            }
+            $this->sqlQuery .= ') ';
+            return $this;
+        }
+        
     }
 
     public function parameters($parameters)
     {
         
-        if (!is_aaray($parameters)){
+        if (!is_array($parameters)){
            $parameters = (array) $parameters;
         }
         $this->parameters = count($parameters);
@@ -67,9 +166,7 @@ class Database
         $i = 0;
         foreach ($parameters as $parameter) {
             $this->sqlQuery .= ' ' . $parameter;
-            if ($i < count($parameters)){
-                $this->sqlQuery .= ', ';
-            }
+            
             $i++;
         }
         $this->sqlQuery .= ') ';
@@ -85,12 +182,11 @@ class Database
         if (!is_aaray($values)){
             $values = (array) $values;
         }
-        $this->connect(); // ar tikrai reikia atidaryti jungti?
         $this->values = count($values);
         $this->sqlQuery .= ' VALUES (';
         $i = 0;
         foreach ($values as $values) {
-            $this->sqlQuery .= ' ' . $this->escape($values);
+            $this->sqlQuery .= ' ' . $values;
             if ($i < count($values)){
                 $this->sqlQuery .= ', ';
             }
@@ -99,15 +195,55 @@ class Database
         $this->sqlQuery .= ') ';
         return $this;
     }
-    
-    public function escape($string) 
+
+    public function innerJoin($tableName)
     {
-        return $this->conn->real_escape_string($string);
+        $this->sqlQuery .= ' INNER JOIN ' . $tableName . ' ';
+        return $this;
+    }
+
+    public function leftJoin($tableName)
+    {
+        $this->sqlQuery .= ' LEFT JOIN ' . $tableName . ' ';
+        return $this;
+    }
+
+    public function rightJoin($tableName)
+    {
+        $this->sqlQuery .= ' RIGHT JOIN ' . $tableName . ' ';
+        return $this;
+    }
+    
+    public function fullOuterJoin($tableName)
+    {
+        $this->sqlQuery .= ' FULL OUTER JOIN ' . $tableName . ' ';
+        return $this;
+    }
+
+    public function orderBy($condition, $sort = 'ASC')
+    {
+        $this->sqlQuery .= ' ORDER BY ' . $condition . ' ' . $sort . ' ';
+        return $this;
+    }
+    public function groupBy($condition)
+    {
+        $this->sqlQuery .= ' GROUP BY ' . $condition . ' ';
+        return $this;
+    }
+    
+    public function on($table1, $table2)
+    {
+        $this->sqlQuery .= ' ON ' . $table1 . ' = ' . $table2 . ' '; 
+        return $this;
     }
 
     public function get()
     {
         $this->connect();
         return $this->conn->query($this->sqlQuery);
+    }
+    public function getSql()
+    {
+        return $this->sqlQuery;
     }
 }
